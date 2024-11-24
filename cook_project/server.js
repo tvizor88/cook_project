dabe8d388a552078e916d0f4ec4ca2c33ae2598a
NBK-278\Aleksandr Gorshkov <aleksandr.gorshkov@chisw.com>
2024-11-24 13:39:29 +0200

    init commit + pages + auth

---
+++ cook_project/server.js
@@ -0,0 +1,187 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/sections', express.static(path.join(__dirname, '..'))); // Добавляем эту строку
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/recipes', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+console.log('Connected to MongoDB');
+});
+
+// Recipe schema and model
+const recipeSchema = new mongoose.Schema({
+title: String,
+section: String,
+description: String,
+steps: [String],
+time: String,
+coverImage: String,
+stepImages: [String]
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+// Настройка хранилища для Multer
+const storage = multer.diskStorage({
+destination: function (req, file, cb) {
+cb(null, 'uploads/');
+},
+filename: function (req, file, cb) {
+cb(null, Date.now() + '-' + file.originalname);
+}
+});
+
+// Настройка фильтра файлов для Multer
+const fileFilter = (req, file, cb) => {
+if (file.mimetype.startsWith('image/')) {
+cb(null, true);
+} else {
+cb(new Error('Invalid file type'), false);
+}
+};
+
+// Настройка Multer для обработки полей формы
+const upload = multer({
+storage: storage,
+fileFilter: fileFilter
+}).fields([
+{ name: 'coverImage', maxCount: 1 },
+{ name: 'stepImages[]', maxCount: 15 }
+]);
+
+// Маршрут для обработки формы
+app.post('/recipes', upload, async (req, res) => {
+try {
+const { title, section, description, steps, time } = req.body;
+const coverImage = req.files['coverImage'] ? `/uploads/${req.files['coverImage'][0].filename}` : '';
+const stepImages = req.files['stepImages[]'] ? req.files['stepImages[]'].map(file => `/uploads/${file.filename}`) : [];
+
+const recipe = new Recipe({ title, section, description, steps: JSON.parse(steps), time, coverImage, stepImages });
+await recipe.save();
+
+// Создание HTML-файла на уровень выше
+const fileName = `${title}.html`;
+const dirPath = path.join(__dirname, '..', 'recipes');
+const filePath = path.join(dirPath, fileName);
+
+// Проверка и создание директории, если она не существует
+if (!fs.existsSync(dirPath)) {
+fs.mkdirSync(dirPath, { recursive: true });
+}
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${title}</title>
+<style>
+body {
+font-family: Arial, sans-serif;
+margin: 0;
+padding: 20px;
+background-color: #f8f8f8;
+}
+header {
+display: flex;
+justify-content: space-between;
+align-items: center;
+background-color: #ff6347;
+color: white;
+padding: 10px;
+}
+header button {
+background-color: white;
+color: #ff6347;
+border: none;
+padding: 10px 20px;
+cursor: pointer;
+}
+header button:hover {
+background-color: #e5533d;
+color: white;
+}
+h1 {
+color: #ff6347;
+}
+.recipe {
+border: 1px solid #ddd;
+padding: 10px;
+margin-bottom: 10px;
+background-color: #fff;
+}
+.recipe img {
+max-width: 200px;
+height: auto;
+margin-left: 20px;
+}
+</style>
+</head>
+<body>
+<header>
+<button onclick="history.back()">Назад</button>
+<h1>${title}</h1>
+<button onclick="location.href='../index.html'">Домой</button>
+</header>
+<div class="recipe">
+${coverImage ? `<img src="http://localhost:3000${coverImage}" alt="${title}" style="max-width: 400px; height: auto;">` : ''}
+<p><strong>Время приготовления:</strong> ${time}</p>
+<p><strong>Ингридиенты:</strong> ${description}</p>
+<p><strong>Шаги приготовления:</strong></p>
+${JSON.parse(steps).map((step, index) => `
+<div>
+<p>Шаг ${index + 1}: ${step}</p>
+${stepImages[index] ? `<img src="http://localhost:3000${stepImages[index]}" alt="Шаг ${index + 1}">` : ''}
+</div>
+`).join('')}
+
+
+</div>
+</body>
+</html>
+`;
+
+fs.writeFile(filePath, htmlContent, (err) => {
+if (err) {
+console.error('Ошибка при создании HTML-файла:', err);
+}
+});
+
+res.json({ message: 'Recipe saved successfully', section });
+} catch (error) {
+console.error('Error saving recipe:', error);
+res.status(500).json({ message: 'Internal Server Error' });
+}
+});
+
+// Маршрут для получения рецептов
+app.get('/recipes', async (req, res) => {
+    try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+    } catch (error) {
+    console.error('Error fetching recipes:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+    }
+    });
+
+// Запуск сервера
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ scripts/scripts.js
@@ -0,0 +1,195 @@
+// Функция аутентификации
+function authenticate() {
+    const username = document.getElementById('authUsername').value;
+    const password = document.getElementById('authPassword').value;
+    
+    if (username === 'admin' && password === 'admin') {
+    localStorage.setItem('auth', JSON.stringify({ role: 'admin', timestamp: new Date().getTime() }));
+    loadPage(true);
+    } else if (username === 'user' && password === 'user') {
+    localStorage.setItem('auth', JSON.stringify({ role: 'user', timestamp: new Date().getTime() }));
+    loadPage(false);
+    } else {
+    document.getElementById('authError').style.display = 'block';
+    }
+    }
+    
+    // Функция загрузки страницы
+    function loadPage(isAdmin) {
+    toggleDisplay('authModal', false);
+    toggleDisplay('header', true);
+    toggleDisplay('nav', true);
+    toggleDisplay('.container', true);
+    
+    if (isAdmin) {
+    toggleDisplay('addRecipeBtn', true);
+    }
+    }
+    
+    // Функция проверки аутентификации
+    function checkAuth() {
+    const auth = JSON.parse(localStorage.getItem('auth'));
+    if (auth) {
+    const currentTime = new Date().getTime();
+    const oneHour = 60 * 60 * 1000;
+    if (currentTime - auth.timestamp < oneHour) {
+    loadPage(auth.role === 'admin');
+    } else {
+    localStorage.removeItem('auth');
+    }
+    }
+    }
+    
+   // Функция выхода из системы
+   function logout() {
+    localStorage.clear();
+    // Скрываем элементы страницы
+    toggleDisplay('header', false);
+    toggleDisplay('nav', false);
+    toggleDisplay('.container', false);
+    // Показываем модальное окно аутентификации
+    const authModal = document.getElementById('authModal');
+    if (authModal) {
+    authModal.style.display = 'block';
+    // Перезагружаем страницу после отображения модального окна
+    setTimeout(() => {
+    location.reload();
+    }, 100); // Небольшая задержка для отображения модального окна
+    } else {
+    // Если модальное окно не найдено, перенаправляем на index.html
+    sessionStorage.setItem('showAuthModal', 'true');
+    window.location.href = 'index.html';
+    }
+    }
+    
+    // Функция переключения отображения элементов
+    function toggleDisplay(selector, show) {
+    const element = document.querySelector(selector) || document.getElementById(selector);
+    if (element) {
+    element.style.display = show ? 'block' : 'none';
+    }
+    }
+    
+    // Проверка авторизации при загрузке страницы
+    window.onload = function() {
+    checkAuth();
+    if (sessionStorage.getItem('showAuthModal') === 'true') {
+    const authModal = document.getElementById('authModal');
+    if (authModal) {
+    authModal.style.display = 'block';
+    }
+    sessionStorage.removeItem('showAuthModal');
+    }
+    };
+    
+    // Прорисовка nav-Panel
+    const addRecipeBtn = document.getElementById('addRecipeBtn');
+    if (addRecipeBtn) {
+    addRecipeBtn.onclick = function() {
+    document.getElementById('myModal').style.display = 'block';
+    };
+    }
+    
+    // Обработчики событий для модальных окон
+    const closeBtn = document.getElementsByClassName('close')[0];
+    if (closeBtn) {
+    closeBtn.onclick = function() {
+    document.getElementById('myModal').style.display = 'none';
+    };
+    }
+    
+    window.onclick = function(event) {
+    const myModal = document.getElementById('myModal');
+    if (event.target == myModal) {
+    myModal.style.display = 'none';
+    }
+    };
+    
+    // Обработчик изменения количества шагов
+    const stepCountInput = document.getElementById('stepCount');
+    if (stepCountInput) {
+    stepCountInput.onchange = function() {
+    const stepCount = parseInt(this.value);
+    const stepsContainer = document.getElementById('stepsContainer');
+    stepsContainer.innerHTML = '';
+    for (let i = 1; i <= stepCount; i++) {
+    const stepDiv = document.createElement('div');
+    stepDiv.innerHTML = `
+    <label for="step${i}">Шаг ${i}:</label><br>
+    <textarea id="step${i}" name="steps[]"></textarea><br><br>
+    <label for="stepImage${i}">Загрузить фото:</label><br>
+    <input type="file" id="stepImage${i}" name="stepImages[]"><br><br>
+    `;
+    stepsContainer.appendChild(stepDiv);
+    }
+    };
+    }
+    
+    // Функция сохранения рецепта
+    async function saveRecipe() {
+    const title = document.getElementById('title').value;
+    const section = document.getElementById('section').value;
+    const shortDescription = document.getElementById('shortDescription').value;
+    const description = document.getElementById('description').value;
+    const steps = Array.from(document.querySelectorAll('textarea[name="steps[]"]')).map(textarea => textarea.value);
+    const time = document.getElementById('time').value;
+    const coverImage = document.getElementById('coverImage').files[0];
+    const stepImages = Array.from(document.querySelectorAll('input[name="stepImages[]"]')).map(input => input.files[0]);
+    
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('section', section);
+    formData.append('shortDescription', shortDescription);
+    formData.append('description', description);
+    formData.append('steps', JSON.stringify(steps));
+    formData.append('time', time);
+    if (coverImage) {
+    formData.append('coverImage', coverImage);
+    }
+    stepImages.forEach((file, index) => {
+    if (file) {
+    formData.append('stepImages[]', file);
+    }
+    });
+    
+    const response = await fetch('http://localhost:3000/recipes', {
+    method: 'POST',
+    body: formData
+    });
+    
+    if (response.ok) {
+    document.getElementById('myModal').style.display = 'none';
+    const result = await response.json();
+    window.location.href = `../${result.section}.html`;
+    } else {
+    console.error('Ошибка при сохранении рецепта');
+    }
+    }
+    
+    // Функция загрузки рецептов
+    async function loadRecipes(section) {
+    const response = await fetch('http://localhost:3000/recipes');
+    const recipes = await response.json();
+    const sectionRecipes = recipes.filter(recipe => recipe.section === section);
+    const recipesContainer = document.getElementById('recipes');
+    
+    sectionRecipes.forEach(recipe => {
+    const recipeDiv = document.createElement('div');
+    recipeDiv.className = 'recipe';
+    recipeDiv.innerHTML = `
+    <div class="recipe-content">
+    <h3><a href="recipes/${recipe.title}.html">${recipe.title}</a></h3>
+    <p><strong>Описание:</strong> ${recipe.description}</p>
+    <p><strong>Время приготовления:</strong> ${recipe.time}</p>
+    </div>
+    ${recipe.coverImage ? `<img src="http://localhost:3000${recipe.coverImage}" alt="${recipe.title}">` : ''}
+    `;
+    recipesContainer.appendChild(recipeDiv);
+    });
+    }
+    
+    // Загрузка рецептов при загрузке страницы
+    document.addEventListener('DOMContentLoaded', function() {
+    const section = document.body.dataset.section;
+    loadRecipes(section);
+    });
