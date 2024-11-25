3ab25262bdfb1864a2d36a5c20469c00bfdadd47
NBK-278\Aleksandr Gorshkov <aleksandr.gorshkov@chisw.com>
2024-11-25 23:29:55 +0200

    updatet git and cover image still FLASH POINT

--- cook_project/server.js
+++ cook_project/server.js
@@ -40,7 +40,7 @@ const Recipe = mongoose.model('Recipe', recipeSchema); // creating the model for
 // Настройка хранилища для Multer
 const storage = multer.diskStorage({
 destination: function (req, file, cb) {
-cb(null, 'uploads/');
+cb(null, '../uploads/');
 },
 filename: function (req, file, cb) {
 cb(null, Date.now() + '-' + file.originalname);
